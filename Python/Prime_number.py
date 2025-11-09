d7a6552a431050f5f692296d8651c7bec764c681
Achala C <chandruachala9@gmail.com>
2025-11-09 22:49:46 +0530

    Improve prime number validation logic
    Refactor prime number check logic for clarity.

--- Python/Prime_number.py
+++ Python/Prime_number.py
@@ -1,10 +1,10 @@
 a=int(input("Input the number"))
 ans = True
-for i in range (2,a//2):
-    if (a==1 or a==0):
-        ans=False 
-        break
-    if (a%i==0):
+if (a<=1):
+    ans=False 
+else:
+    for i in range (2,a//2):
+        if (a%i==0):
         ans=False
         break
 if(ans):
